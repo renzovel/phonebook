9152ab66c9425a415bcdd8cbfa386002fa07d5ba
Renzo Veliz <renzo.veliz1br@gmail.com>
2022-06-20 09:18:37 -0300

    modificado o componente header para colocar o icone

--- src/component/view/Header.js
+++ src/component/view/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Logo from '../../asset/images/logo192.png';
 function Header(){
     return (
         <header className='Header'>
             <div className='logo-container'>
-                <img src='' alt='Logo'/>
+                <img src={Logo} alt='Phone Book' title='Phone Book'/>
             </div>
             <div className='search-container'>
                 
