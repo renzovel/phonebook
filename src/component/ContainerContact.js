47592bd2807ebf5f08959ad6b231f3a369e69369
Renzo Veliz <renzo.veliz1br@gmail.com>
2022-06-22 18:24:11 -0300

    criada uma tabla mas so com div e flex css

--- src/component/ContainerContact.js
+++ src/component/ContainerContact.js
@@ -1,8 +1,41 @@
 function ContainerContact(){
     return(
         <div className='ContainerContact'>
-            <div className="">
-                
+            <div className="container-table">
+                <div className='row-table'>
+                    <div>
+                        <img src="https://ui-avatars.com/api/?background=227aee&name=Renzo+Veliz&color=FFFF" alt="Renzo Veliz" title="Renzo Veliz" />
+                    </div>
+                    <div>
+                    Elisama Camacho Maranhão Elisama
+                    </div>
+                    <div>
+                        +55(92)000000000
+                    </div>
+                    <div>
+                        elisama123@exemplo.com
+                    </div>
+                    <div>
+                        opciones
+                    </div>
+                </div>  
+                <div className='row-table'>
+                    <div>
+                        <img src="https://ui-avatars.com/api/?background=227aee&name=Renzo+Veliz&color=FFFF" alt="Renzo Veliz" title="Renzo Veliz" />
+                    </div>
+                    <div>
+                    Elisama Camacho Maranhão Elisama
+                    </div>
+                    <div>
+                        +55(92)000000000
+                    </div>
+                    <div>
+                        elisama123@exemplo.com
+                    </div>
+                    <div>
+                        opciones
+                    </div>
+                </div>              
             </div>
         </div>
     )
