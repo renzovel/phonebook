5674b88477062112124e360659e9c9e62418d255
Renzo Veliz <renzo.veliz1br@gmail.com>
2022-06-28 12:54:32 -0300

    criado o segundo boton de agregar contato

--- src/component/MenuLeft.js
+++ src/component/MenuLeft.js
@@ -1,6 +1,12 @@
-function MenuLeft(){
+import {Button} from 'react-bootstrap';
+function MenuLeft(props){
     return(
         <aside className='MenuLeft'>
+            <div className="d-grid gap-2">
+                <Button className='btn-criar-contacts' variant="success" size="lg">
+                    Criar Contato
+                </Button>
+            </div>
         </aside>
     )
 }
